59d0444fae19835fa4bd7f9087f46860070e448d
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-09-17 21:02:22 +0700

    ProjectConf: configuration for project

--- src/main/java/com/example/library_management/LibraryManagementApplication.java
+++ src/main/java/com/example/library_management/LibraryManagementApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryManagementApplication {
-
-	public static void main(String[] args) {
+public class LibraryManagementApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(LibraryManagementApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/library_management/config/ProjectConf.java
@@ -0,0 +1,11 @@
+package com.example.library_management.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+class ProjectConf
+{
+
+}
