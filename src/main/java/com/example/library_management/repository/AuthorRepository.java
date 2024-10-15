71e1982456e8439a1aa55ef208b0789374680a9f
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-15 10:41:49 +0700

    Test query

--- src/main/java/com/example/library_management/repository/AuthorRepository.java
+++ src/main/java/com/example/library_management/repository/AuthorRepository.java
@@ -2,5 +2,9 @@ package com.example.library_management.repository;
 
 import com.example.library_management.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AuthorRepository extends JpaRepository<Author, Integer> {}
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+  @Query("select a from Author a where a.name = :name")
+  Author findByName(String name);
+}
