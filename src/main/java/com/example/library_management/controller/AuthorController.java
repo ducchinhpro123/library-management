4f0e0f0c657880e40f77055a4f380324e9bf00a1
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-15 08:42:37 +0700

    New classes for handle retrieve and handle for table author

---
+++ src/main/java/com/example/library_management/controller/AuthorController.java
@@ -0,0 +1,63 @@
+package com.example.library_management.controller;
+
+import com.example.library_management.model.Author;
+import com.example.library_management.service.AuthorService;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class AuthorController {
+  private final AuthorService authorService;
+
+  public AuthorController(AuthorService authorService) {
+    this.authorService = authorService;
+  }
+
+  @PostMapping("/create-author")
+  public ResponseEntity<String> createAuthor(@RequestBody Author newAuthor) {
+    try {
+      authorService.createAuthor(newAuthor);
+      return new ResponseEntity<>("Author created successfully.", HttpStatus.CREATED);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @DeleteMapping("/delete-author")
+  public ResponseEntity<String> deleteBook(@RequestParam(name = "isbn") Integer id) {
+    try {
+      authorService.deleteAuthor(id);
+      return new ResponseEntity<>("Author deleted successfully.", HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @PostMapping("/edit-author")
+  public ResponseEntity<String> updateBook(@RequestBody Author author) {
+    try {
+      Integer id = author.getId();
+      authorService.updateAuthor(id, author);
+      return new ResponseEntity<>("Author updated successfully.", HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @GetMapping("/get-author")
+  public ResponseEntity<?> getBook(@RequestParam(name = "id") Integer id) {
+    Optional<Author> author = authorService.getAuthor(id);
+    if (author.isPresent()) return new ResponseEntity<>(author.get(), HttpStatus.OK);
+    else {
+      Map<String, String> message = new HashMap<>();
+      message.put("message", "Can not found the author with the id: " + id.toString());
+      return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
---
+++ src/main/java/com/example/library_management/service/AuthorService.java
@@ -0,0 +1,53 @@
+package com.example.library_management.service;
+
+import com.example.library_management.model.Author;
+import com.example.library_management.repository.AuthorRepository;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+  private final AuthorRepository authorRepository;
+
+  public AuthorService(AuthorRepository authorRepository) {
+    this.authorRepository = authorRepository;
+  }
+
+  public void createAuthor(Author newAuthor) throws IllegalArgumentException {
+    if (newAuthor == null) return;
+
+    try {
+      authorRepository.save(newAuthor);
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage());
+    }
+  }
+
+  public void deleteAuthor(Integer id) throws IllegalArgumentException {
+    try {
+      authorRepository.deleteById(id);
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage());
+    }
+  }
+
+  public void updateAuthor(Integer id, Author newAuthor) throws IllegalArgumentException {
+    Optional<Author> bookOptional = authorRepository.findById(id);
+    if (bookOptional.isPresent()) {
+      Author author = bookOptional.get();
+      author.setName(newAuthor.getName());
+      author.setDescription(newAuthor.getDescription());
+      authorRepository.save(author);
+    } else {
+      throw new RuntimeException("Can not update author");
+    }
+  }
+
+  public Optional<Author> getAuthor(Integer id) {
+    Optional<Author> author = authorRepository.findById(id);
+    if (author.isEmpty()) {
+      return Optional.empty();
+    }
+    return author;
+  }
+}
