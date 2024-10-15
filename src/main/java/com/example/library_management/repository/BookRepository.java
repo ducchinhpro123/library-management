a605dd029a7e6d2c796f9693102b214e4c23e34e
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-15 10:42:11 +0700

    For handle many to many relationship, need to save in database

--- src/main/java/com/example/library_management/repository/BookRepository.java
+++ src/main/java/com/example/library_management/repository/BookRepository.java
@@ -2,5 +2,11 @@ package com.example.library_management.repository;
 
 import com.example.library_management.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface BookRepository extends JpaRepository<Book, String> {}
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, String> {
+    List<Book> findByAuthorsName(@Param("authorName") String authorName);
+}
