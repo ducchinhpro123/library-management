c57f5a6688c19e6c2646f0434f2598216fa3427a
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-23 12:23:50 +0700

    Controller for handling request from browser

--- src/main/java/com/example/library_management/controller/BookController.java
+++ src/main/java/com/example/library_management/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController
+@RestController("/api")
 public class BookController  {
   private final BookService bookService;
 
--- src/main/java/com/example/library_management/controller/MainController.java
+++ src/main/java/com/example/library_management/controller/MainController.java
@@ -1,7 +1,76 @@
 package com.example.library_management.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+import com.example.library_management.model.Book;
+import com.example.library_management.service.BookService;
+
+import java.util.List;
+
+
+@Controller
 public class MainController {
+
+  private final BookService bookService;
+
+  public MainController(BookService bookService) {
+    this.bookService = bookService;
+  }
+
+  @GetMapping("/")
+  public String homePage(Model model ) {
+    List<Book> books = bookService.findAllBooks();
+    model.addAttribute("books", books);
+    return "index";
+  }
+
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
+
+  //@PostMapping("/create-book")
+  //public ResponseEntity<String> createBook(@RequestBody Book newBook, @RequestParam String authorName) {
+  //  try {
+  //    bookService.createBook(newBook, authorName);
+  //    return new ResponseEntity<>("Book created successfully.", HttpStatus.CREATED);
+  //  } catch (Exception e) {
+  //    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  //  }
+  //}
+  //
+  //@DeleteMapping("/delete-book")
+  //public ResponseEntity<String> deleteBook(@RequestParam(name = "isbn") String isbn) {
+  //  try {
+  //    bookService.deleteBook(isbn);
+  //    return new ResponseEntity<>("Book deleted successfully.", HttpStatus.OK);
+  //  } catch (Exception e) {
+  //    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  //  }
+  //}
+  //
+  //@PutMapping("/edit-book")
+  //public ResponseEntity<String> updateBook(@RequestBody Book book) {
+  //  try {
+  //    String isbn = book.getISBN();
+  //    bookService.updateBook(isbn, book);
+  //    return new ResponseEntity<>("Book updated successfully.", HttpStatus.OK);
+  //  } catch (Exception e) {
+  //    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  //  }
+  //}
+  //
+  //@GetMapping("/get-book")
+  //public ResponseEntity<?> getBook(@RequestParam(name = "isbn") String isbn) {
+  //  Optional<Book> book = bookService.getBook(isbn);
+  //  if (book.isPresent()) return new ResponseEntity<>(book.get(), HttpStatus.OK);
+  //  else {
+  //    Map<String, String> message = new HashMap<>();
+  //    message.put("message", "Can not found the book with the isbn: " + isbn);
+  //    return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+  //  }
+  //}
+
 }
