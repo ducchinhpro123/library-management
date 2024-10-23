c3e6e594202ac7bf47653e738905cd83c14c9fe1
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-23 12:24:13 +0700

    Create an Account model

---
+++ src/main/java/com/example/library_management/model/Account.java
@@ -0,0 +1,16 @@
+package com.example.library_management.model;
+
+//import java.util.LinkedHashSet;
+//import java.util.Set;
+
+//import jakarta.persistence.Entity;
+
+//@Entity 
+
+public class Account {
+
+  private String username;
+  private String password;
+  private String status;
+
+}
