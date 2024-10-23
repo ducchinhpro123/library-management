fb49179d4ed255f1088e079149eec4dff4c8a351
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-23 12:23:35 +0700

    Configure security

---
+++ src/main/java/com/example/library_management/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.library_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            (requests) ->
+                requests
+                    .requestMatchers("/", "/assets/**", "/images/**", "/login")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .formLogin((form) -> form.loginPage("/login").permitAll())
+        .logout((logout) -> logout.permitAll());
+
+    return http.build();
+  }
+}
