e3fcd73dc9173d8fc0d494c0f88e19dc6bc9dc6e
djksanrs <chinh99562@donga.edu.vn>
2024-11-13 10:11:52 +0700

    Implement logic to handle view Book in detail and save it in Service class

--- src/main/java/com/example/library_management/controller/BookController.java
+++ src/main/java/com/example/library_management/controller/BookController.java
@@ -1,27 +1,96 @@
 package com.example.library_management.controller;
 
+import com.example.library_management.dto.BookDTO;
+import com.example.library_management.model.Author;
 import com.example.library_management.model.Book;
+import com.example.library_management.model.Subject;
+import com.example.library_management.repository.SubjectRepository;
+import com.example.library_management.service.AuthorService;
+import com.example.library_management.service.BookAuthorService;
 import com.example.library_management.service.BookService;
-
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
-
-@RestController("/api")
-public class BookController  {
+// @RestController("/api")
+@Controller
+public class BookController {
   private final BookService bookService;
+  private final BookAuthorService bookAuthorService;
+  private final SubjectRepository subjectRepository;
+  private final AuthorService authorService;
 
-  public BookController(BookService bookService) {
+  public BookController(BookService bookService, BookAuthorService bookAuthorService, SubjectRepository subjectRepository, AuthorService authorService) {
     this.bookService = bookService;
+    this.bookAuthorService = bookAuthorService;
+      this.subjectRepository = subjectRepository;
+    this.authorService = authorService;
+  }
+
+  /* The table page summary books */
+  @GetMapping("/table")
+  public String tablePage(Model model) {
+    List<Book> books = bookService.findAllBooks();
+    model.addAttribute("books", books);
+    return "table";
+  }
+
+  @GetMapping("/new-book")
+  public String newBookPage(Model model) {
+    model.addAttribute("book", new BookDTO());
+    model.addAttribute("subjects", subjectRepository.findAll());
+    return "new_book";
+  }
+
+  @PostMapping("/new-book")
+  public String newBook(@ModelAttribute BookDTO bookDTO, Model model) {
+    try {
+      bookService.saveBookWithoutAuthor(bookDTO);
+      model.addAttribute("message", "Book created successfully.");
+      return "redirect:/table";
+    } catch(IllegalArgumentException e) {
+        model.addAttribute("message", "Invalid input: " + e.getMessage());
+        return "redirect:/new-book";
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      model.addAttribute("message", e.getMessage());
+      return "redirect:/new-book";
+    }
+  }
+
+
+
+  @GetMapping("/book/{isbn}") // 
+  public String viewDetailBook(@PathVariable("isbn") String isbn, Model model) {
+    Optional<Book> book = bookService.getBook(isbn);
+    // th:if="${book}"
+    if (book.isPresent()) {
+      //System.out.println(book.get().getTitle());
+      Optional<Author> author = bookAuthorService.findAuthorByBookIsbn(isbn);
+      if (author.isPresent()) {
+        model.addAttribute("author", author.get());
+      } else {
+        model.addAttribute("author", null);
+      }
+      model.addAttribute("book", book.get());
+    } else {
+      // th:if="${message}"
+      model.addAttribute("message", "Can not found any book with that isbn: " + isbn);
+    }
+
+    return "book_detail";
   }
 
   @PostMapping("/create-book")
-  public ResponseEntity<String> createBook(@RequestBody Book newBook, @RequestParam String authorName) {
+  public ResponseEntity<String> createBook(
+      @RequestBody Book newBook, @RequestParam String authorName) {
     try {
       bookService.createBook(newBook, authorName);
       return new ResponseEntity<>("Book created successfully.", HttpStatus.CREATED);
