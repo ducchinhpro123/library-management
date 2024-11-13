43fee65faa58938a46bca14d573d0eff259861cc
djksanrs <chinh99562@donga.edu.vn>
2024-11-13 17:29:04 +0700

    Separate logics into Service

--- src/main/java/com/example/library_management/controller/AccountController.java
+++ src/main/java/com/example/library_management/controller/AccountController.java
@@ -1,13 +1,7 @@
 package com.example.library_management.controller;
 
 import com.example.library_management.model.Account;
-import com.example.library_management.model.AccountRole;
-import com.example.library_management.model.AccountStatus;
-import com.example.library_management.model.Member;
-import com.example.library_management.repository.LibrarianRepository;
-import com.example.library_management.repository.MemberRepository;
-import java.util.Optional;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.example.library_management.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,17 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AccountController {
 
-  private final PasswordEncoder passwordEncoder;
-  private final LibrarianRepository librarianRepository;
-  private final MemberRepository memberRepository;
+  private final AccountService accountService;
 
-  public AccountController(
-      LibrarianRepository librarianRepository,
-      PasswordEncoder passwordEncoder,
-      MemberRepository memberRepository) {
-    this.librarianRepository = librarianRepository;
-    this.memberRepository = memberRepository;
-    this.passwordEncoder = passwordEncoder;
+  public AccountController(AccountService accountService) {
+    this.accountService = accountService;
   }
 
   @GetMapping("/registration")
@@ -37,23 +24,11 @@ public class AccountController {
 
   @PostMapping("/registration")
   public String registerAccountHandle(Model model, Account account) {
-    String username = account.getUsername();
-    Optional<Member> memberOption = memberRepository.getMemberByUsername(username);
-
-    if (memberOption.isEmpty()) {
-      Member member = new Member();
-      member.setUsername(account.getUsername());
-      member.addRole(AccountRole.MEMBER);
-      member.setPassword(passwordEncoder.encode(account.getPassword()));
-      member.setStatus(AccountStatus.ACTIVE);
-
-      memberRepository.save(member);
-
+    try {
+      accountService.saveAccount(account);
       return "redirect:/login";
-
-    } else {
-      model.addAttribute(
-          "userNameAlreadyExist", "username already taken, please choose another username");
+    } catch (Exception e) {
+      model.addAttribute("message", e.getMessage());
       return "register";
     }
   }
