9c403e095aa31609bc9024e544e371f2d4e5dd44
djksanrs <chinh99562@donga.edu.vn>
2024-11-13 10:09:39 +0700

    Reformat the file and adjust method call

--- src/main/java/com/example/library_management/controller/AccountController.java
+++ src/main/java/com/example/library_management/controller/AccountController.java
@@ -1,20 +1,18 @@
 package com.example.library_management.controller;
 
-import java.util.Optional;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.example.library_management.model.Account;
 import com.example.library_management.model.AccountRole;
 import com.example.library_management.model.AccountStatus;
 import com.example.library_management.model.Member;
 import com.example.library_management.repository.LibrarianRepository;
 import com.example.library_management.repository.MemberRepository;
+import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AccountController {
@@ -23,8 +21,10 @@ public class AccountController {
   private final LibrarianRepository librarianRepository;
   private final MemberRepository memberRepository;
 
-  public AccountController(LibrarianRepository librarianRepository, PasswordEncoder passwordEncoder,
-    MemberRepository memberRepository) {
+  public AccountController(
+      LibrarianRepository librarianRepository,
+      PasswordEncoder passwordEncoder,
+      MemberRepository memberRepository) {
     this.librarianRepository = librarianRepository;
     this.memberRepository = memberRepository;
     this.passwordEncoder = passwordEncoder;
@@ -37,25 +37,24 @@ public class AccountController {
 
   @PostMapping("/registration")
   public String registerAccountHandle(Model model, Account account) {
-    String username                 = account.getUsername();
-    Optional<Member> memberOption   = memberRepository.getMemberByUsername(username);
+    String username = account.getUsername();
+    Optional<Member> memberOption = memberRepository.getMemberByUsername(username);
 
-    if (!memberOption.isPresent()) {
+    if (memberOption.isEmpty()) {
       Member member = new Member();
       member.setUsername(account.getUsername());
-
       member.addRole(AccountRole.MEMBER);
-
       member.setPassword(passwordEncoder.encode(account.getPassword()));
       member.setStatus(AccountStatus.ACTIVE);
 
       memberRepository.save(member);
 
-      return "redirect:/home";
+      return "redirect:/login";
 
     } else {
-      model.addAttribute("userNameAlreadyExist", "username already taken, please choose another username");
-      return "registration";
+      model.addAttribute(
+          "userNameAlreadyExist", "username already taken, please choose another username");
+      return "register";
     }
   }
 
@@ -67,5 +66,4 @@ public class AccountController {
 
     return "login";
   }
-
 }
---
+++ src/main/java/com/example/library_management/repository/SubjectRepository.java
@@ -0,0 +1,4 @@
+package com.example.library_management.repository;
+
+public interface SubjectRepository {
+}
