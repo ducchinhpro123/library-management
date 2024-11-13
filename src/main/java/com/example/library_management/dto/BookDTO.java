fce2e50b13d1c6b4fe4c893db0070b9d61a69b69
djksanrs <chinh99562@donga.edu.vn>
2024-11-13 10:12:39 +0700

    Create BookDTO that reflect the Book for more robust

--- src/main/java/com/example/library_management/dto/BookDTO.java
+++ src/main/java/com/example/library_management/dto/BookDTO.java
@@ -1,4 +1,119 @@
 package com.example.library_management.dto;
 
+import com.example.library_management.model.Author;
+import com.example.library_management.model.BookItem;
+import com.example.library_management.model.Subject;
+import jakarta.persistence.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class BookDTO {
+    @Id
+    private String ISBN;
+    private String title;
+
+    @OneToMany(mappedBy="book")
+    private Set<Subject> subjects = new LinkedHashSet<>();
+
+    private String publisher;
+    private String language;
+    private int numberOfPage;
+
+    @Lob
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    private MultipartFile image;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @OneToMany(mappedBy = "book")
+    private Set<BookItem> bookItems;
+
+    public Set<BookItem> getBookItems() {
+        return bookItems;
+    }
+
+    public void setBookItems(Set<BookItem> bookItems) {
+        this.bookItems = bookItems;
+    }
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new LinkedHashSet<>();
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String iSBN) {
+        ISBN = iSBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getNumberOfPage() {
+        return numberOfPage;
+    }
+
+    public void setNumberOfPage(int numberOfPage) {
+        this.numberOfPage = numberOfPage;
+    }
+
+    public Set<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Set<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    public MultipartFile getImage() {
+        return image;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
+    }
 }
