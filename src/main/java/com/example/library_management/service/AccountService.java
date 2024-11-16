89e21fde53876a928ee4ec9eab7c33c35af76b6b
djksanrs <chinh99562@donga.edu.vn>
2024-11-16 09:46:47 +0700

    Implement logic to save different kind of roles

--- src/main/java/com/example/library_management/service/AccountService.java
+++ src/main/java/com/example/library_management/service/AccountService.java
@@ -1,12 +1,14 @@
 package com.example.library_management.service;
 
-import com.example.library_management.model.Account;
-import com.example.library_management.model.AccountRole;
-import com.example.library_management.model.AccountStatus;
-import com.example.library_management.model.Member;
+import com.example.library_management.model.*;
+import com.example.library_management.repository.AdminRepository;
 import com.example.library_management.repository.LibrarianRepository;
 import com.example.library_management.repository.MemberRepository;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +17,75 @@ public class AccountService {
 
   private final MemberRepository memberRepository;
   private final LibrarianRepository librarianRepository;
+  private final AdminRepository adminRepository;
   private final PasswordEncoder passwordEncoder;
 
   public AccountService(
       PasswordEncoder passwordEncoder,
       MemberRepository memberRepository,
+      AdminRepository adminRepository,
       LibrarianRepository librarianRepository) {
     this.memberRepository = memberRepository;
-    this.librarianRepository = librarianRepository;
     this.passwordEncoder = passwordEncoder;
+    this.adminRepository = adminRepository;
+    this.librarianRepository = librarianRepository;
+  }
+
+  public void saveLibrarian(Account account) throws Exception {
+    String username = account.getUsername();
+    Optional<Librarian> librarianOpt = librarianRepository.getLibrarianByUsername(username);
+
+    if (librarianOpt.isEmpty()) {
+      Librarian lib = new Librarian();
+
+      lib.setUsername(account.getUsername());
+      lib.addRole(AccountRole.LIBRARIAN); // Default
+      lib.setPassword(passwordEncoder.encode(account.getPassword()));
+      lib.setStatus(AccountStatus.ACTIVE);
+
+      try {
+        librarianRepository.save(lib);
+      } catch (Exception e) {
+        throw new Exception(e.getMessage());
+      }
+    } else {
+      throw new Exception("Account with username " + username + " already exists.");
+    }
+  }
+
+  public void saveAdmin(Account account) throws Exception {
+    String username = account.getUsername();
+    Optional<Admin> adminOpt = adminRepository.getAdminByUsername(username);
+
+    if (adminOpt.isEmpty()) {
+      Admin admin = new Admin();
+
+      admin.setUsername(account.getUsername());
+      admin.addRole(AccountRole.ADMIN); // Default
+      admin.setPassword(passwordEncoder.encode(account.getPassword()));
+      admin.setStatus(AccountStatus.ACTIVE);
+
+      try {
+        adminRepository.save(admin);
+      } catch (Exception e) {
+        throw new Exception(e.getMessage());
+      }
+    } else {
+      throw new Exception("Account with username " + username + " already exists.");
+    }
+  }
+
+  public void saveAccountWithPermission(Account account) throws Exception {
+    Set<AccountRole> roles = account.getRoles();
+    if (roles.contains(AccountRole.ADMIN)) {
+      saveAdmin(account);
+    } else if (roles.contains(AccountRole.MEMBER)) {
+      saveAccount(account);
+    } else if (roles.contains(AccountRole.LIBRARIAN)){
+      saveLibrarian(account);
+    } else {
+        throw new Exception("Invalid role.");
+    }
   }
 
   public void saveAccount(Account account) throws Exception {
@@ -33,7 +95,7 @@ public class AccountService {
     if (memberOption.isEmpty()) {
       Member member = new Member();
       member.setUsername(account.getUsername());
-      member.addRole(AccountRole.MEMBER);
+      member.addRole(AccountRole.MEMBER); // Default
       member.setPassword(passwordEncoder.encode(account.getPassword()));
       member.setStatus(AccountStatus.ACTIVE);
 
