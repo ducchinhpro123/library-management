6f2a5fdc290f7dd8426da4ee0f7db07e57358b3f
djksanrs <chinh99562@donga.edu.vn>
2024-11-18 23:04:59 +0700

    Add more fields that are not used by default unless user choose to use it

--- src/main/java/com/example/library_management/model/Account.java
+++ src/main/java/com/example/library_management/model/Account.java
@@ -9,6 +9,9 @@ import jakarta.persistence.MappedSuperclass;
 public class Account {
   private String username;
   private String password;
+  private String firstName;
+  private String lastName;
+  private String image;
 
   private Set<AccountRole> roles;
 
@@ -54,4 +57,27 @@ public class Account {
     roles.add(role);
   }
 
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+  
+  public String getLastName() {
+    return lastName;
+  }
+  
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+  
+  public String getImage() {
+    return image;
+  }
+  
+  public void setImage(String image) {
+    this.image = image;
+  }
 }
