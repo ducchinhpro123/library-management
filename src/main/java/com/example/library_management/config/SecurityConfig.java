4f39ea95fa952077f4bb7ee3eb206887e771e8a8
djksanrs <chinh99562@donga.edu.vn>
2024-12-16 22:26:36 +0700

    Custom messages to /login?error=true and message follow by it

--- src/main/java/com/example/library_management/config/SecurityConfig.java
+++ src/main/java/com/example/library_management/config/SecurityConfig.java
@@ -1,18 +1,32 @@
 package com.example.library_management.config;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -29,15 +43,46 @@ public class SecurityConfig {
       .authorizeHttpRequests(
             (requests) ->
                 requests
-                    .requestMatchers("/admin/**", "/blank").hasRole("ADMIN")
                     // .requestMatchers("/profile").hasAnyAuthority("ROLE_ADMIN", "ROLE_MEMBER", "ROLE_LIBRARIAN")
+                    .requestMatchers("/admin/**", "/blank").hasRole("ADMIN")
                     .requestMatchers("/", "/registration", "/assets/**", "/images/**", "/login", "/book/**", "api/book/filter/*")
                     .permitAll()
                     .anyRequest()
                     .authenticated()).exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"))
-        .formLogin((form) -> form.loginPage("/login").defaultSuccessUrl("/").permitAll())
+        .formLogin((form) -> form.loginPage("/login")
+            .defaultSuccessUrl("/")
+            .failureUrl("/login?error=true")
+            .failureHandler(authenticationFailureHander())
+            .permitAll())
         .logout(LogoutConfigurer::permitAll)
         .build();
+  }
+
+  @Bean
+  public AuthenticationFailureHandler authenticationFailureHander() {
+    return new SimpleUrlAuthenticationFailureHandler() {
+      @Override
+      public void onAuthenticationFailure(HttpServletRequest request, 
+          HttpServletResponse response,
+          AuthenticationException exception) throws IOException, ServletException {
+        String errorMessage = "Invalid username or password";
+        if (exception instanceof BadCredentialsException) {
+          errorMessage = "Invalid username or password";
+        } else if (exception instanceof LockedException) {
+          errorMessage = "Account is locked";
+        } else if (exception instanceof DisabledException) {
+          errorMessage = "Account is disabled";
+        } else if (exception instanceof AccountExpiredException) {
+          errorMessage = "Account has expired";
+        }
+
+        String redirectUrl = "/login?error=true&&message=" +
+          URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
+        setDefaultFailureUrl(redirectUrl);
+        super.onAuthenticationFailure(request, response, exception);
+
+      }
+    };
   }
 
   @Bean
