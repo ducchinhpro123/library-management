336ce5e3920d879d88a647c82a15758b7a3c879d
djksanrs <chinh99562@donga.edu.vn>
2024-12-17 13:12:29 +0700

    Update validator

--- src/main/java/com/example/library_management/validator/AccountValidator.java
+++ src/main/java/com/example/library_management/validator/AccountValidator.java
@@ -2,8 +2,10 @@ package com.example.library_management.validator;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 
 import com.example.library_management.model.Account;
+import com.example.library_management.model.ProfileUpdateDTO;
 
 
 @Component
@@ -14,17 +16,49 @@ public class AccountValidator implements org.springframework.validation.Validato
     return Account.class.equals(clazz);
   }
 
+  private void validateAccount(Account account, Errors errors) {
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "field.required");
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "field.required");
+
+    if (account.getFirstName() != null && 
+        (account.getFirstName().length() < 2 || account.getFirstName().length() > 50)) {
+      errors.rejectValue("firstName", "size.account.firstName");
+        }
+
+    if (account.getLastName() != null && 
+        (account.getLastName().length() < 2 || account.getLastName().length() > 50)) {
+      errors.rejectValue("lastName", "size.account.lastName");
+        }
+  }
+
+  private void validateProfileDTO(ProfileUpdateDTO profile, Errors errors) {
+    if (profile.getFirstName() != null && 
+        (profile.getFirstName().length() < 2 || profile.getFirstName().length() > 50)) {
+      errors.rejectValue("firstName", "size.profile.firstName");
+        }
+
+    if (profile.getLastName() != null && 
+        (profile.getLastName().length() < 2 || profile.getLastName().length() > 50)) {
+      errors.rejectValue("lastName", "size.profile.lastName");
+        }
+  }
+
+  private void validateProfileUpdate(ProfileUpdateDTO profileUpdate, Errors errors) {
+    if (profileUpdate.getFirstName() != null && profileUpdate.getFirstName().length() < 2) {
+      errors.rejectValue("firstName", "firstName.tooShort",  "First name must be at least 2 characters");
+    }
+  }
+
   @Override
   public void validate(Object target, Errors errors) {
-    Account account = (Account) target;
-   
-    System.out.println("METHOD IS CALLED");
+    
+    System.out.println(target);
 
-    if (account.getPassword() != null && account.getPasswordConfirm() != null) {
-      if (!account.getPassword().equals(account.getPasswordConfirm())) {
-        errors.rejectValue("passwordConfirm", "password.mismatch", "Passwords do not match");
-      }
-    }
+    if (target instanceof Account) {
+      validateAccount((Account) target, errors);
+    } else if (target instanceof ProfileUpdateDTO) {
+      validateProfileDTO((ProfileUpdateDTO) target, errors);
+    } 
   }
 
 }
